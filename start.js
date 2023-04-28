285a579ed6fe737dba37c154e5fb0c19f33d7863
Artem Poddieniezhniy <45436105+boomerango@users.noreply.github.com>
2023-04-28 13:51:28 +0300

    Update start.js

--- start.js
+++ start.js
@@ -10,4 +10,4 @@ function printString(n) {
 }
 
 // Example usage:
-printString(1*60*1000); // Prints "Hello, world!" every second for 5 seconds
+printString(20*60*1000); // Prints "Hello, world!" every second for 5 seconds
