4ec68545bee3b67d86be65feb83b87bc0d88cecb
Artem Poddieniezhniy <45436105+boomerango@users.noreply.github.com>
2023-06-14 12:00:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <h1>React Quickstart</h1>
-        <p>A template project deployed to TelemetryTV Pages</p>
+        <p>A template project deployed TTV webapps</p>
 
         <GHBadge username="MichaelCurrin" repoName="react-quickstart" />
       </header>
